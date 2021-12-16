5289de68daa40c44297e811bcd82d00494d176be
Nicolas Mattia <nicolas@nmattia.com>
2021-12-16 19:31:29 +0100

    Fix nodejs URL for x86_64

--- pkgs/nodejs.nix
+++ pkgs/nodejs.nix
@@ -2,7 +2,7 @@
 let
   urls = {
     aarch64-darwin = https://nodejs.org/dist/v16.13.1/node-v16.13.1-darwin-arm64.tar.gz;
-    x86_64-darwin = https://nodejs.org/dist/v16.13.1/node-v16.13.1-darwin-x86.tar.gz;
+    x86_64-darwin = https://nodejs.org/dist/v16.13.1/node-v16.13.1-darwin-x64.tar.gz;
   };
   src = builtins.fetchTarball { url = urls."${builtins.currentSystem}"; name = "nodejs"; };
 
